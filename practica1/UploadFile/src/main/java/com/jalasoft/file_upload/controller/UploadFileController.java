f47d408d3e86fe7703ebc686240e3ffb60e455cc
dxni <33109901+dxni@users.noreply.github.com>
2020-05-09 23:20:01 -0400

    Copy Right
    add copyrigth

--- practica1/UploadFile/src/main/java/com/jalasoft/file_upload/controller/UploadFileController.java
+++ practica1/UploadFile/src/main/java/com/jalasoft/file_upload/controller/UploadFileController.java
@@ -1,3 +1,14 @@
+/*
+ *
+ *  * Copyright (c) 2020 Jalasoft.
+ *  *
+ *  * This software is the confidential and proprietary information of Jalasoft.
+ *  * ("Confidential Information"). You shall not disclose such Confidential
+ *  * Information and shall use it only in accordance with the terms of the
+ *  * license agreement you entered into with Jalasoft.
+ *
+ */
+
 package com.jalasoft.file_upload.controller;
 
 import com.jalasoft.file_upload.service.UploadFileService;
